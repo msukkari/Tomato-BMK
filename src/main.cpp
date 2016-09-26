ee3a90248333029f8bacc1a4a569c65d3ce97246
msukkarieh <michael.sukkarieh@mail.mcgill.ca>
2016-09-26 19:51:26 -0400

    modified main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
 
 int main (){
 	printf("hello world!\n");
+
+	std::string test;
+	std::cin >> test; //Waits for input so the screen doesn't close
 	return 0;
 }
