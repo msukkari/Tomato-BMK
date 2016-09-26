3549e2c6134f60c3e764e6191076026c14a74921
Brendan <bshizzle1234@gmail.com>
2016-09-26 19:52:35 -0400

    Added test function

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #include <iostream>
 #include <string>
 
+
+// sum two integers
+int add(int a, int b){
+    return a + b;
+}
+
 int main (){
 	printf("hello world!\n");
 
 	std::string test;
 	std::cin >> test; //Waits for input so the screen doesn't close
+    
+    
+    printf("7 + 9 = %d", add(7, 9));
+    
 	return 0;
 }
